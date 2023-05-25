2012be0202fbd346b455ad253f6be686f5a7e554
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-25 19:35:24 +0300

    Configuration de projet pour connecter au BD postgresql et migration

--- disquaire_project/disquaire_project/settings.py
+++ disquaire_project/disquaire_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'disquaire_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'disquaires',
+        'USER': 'postgres',
+        'PASSWORD': 'AllahSeul',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
